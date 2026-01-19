9b9ca2cefe7cf19694e5ff79983c7b1605d5ad3a
CHAEGODA <chae_goda@naver.com>
2026-01-19 10:26:14 +0900

    init: fitline 스프링 부트 프로젝트 초기 세팅

---
+++ src/main/java/com/thunder11/fitline_be/FitlineBeApplication.java
@@ -0,0 +1,13 @@
+package com.thunder11.fitline_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlineBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlineBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thunder11/fitline_be/FitlineBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thunder11.fitline_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlineBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
