81bde9f4b6d01bc9ab40e95e0e0e1cdcf9f96057
CHAEGODA <chae_goda@naver.com>
2026-01-30 15:11:42 +0900

    chore: AI 분석요청 로직 수정

--- src/main/java/com/thunder11/scuad/jobposting/repository/AiEvalJobRepository.java
+++ src/main/java/com/thunder11/scuad/jobposting/repository/AiEvalJobRepository.java
@@ -8,7 +8,10 @@ import com.thunder11.scuad.jobposting.domain.AiEvalJob;
 import com.thunder11.scuad.jobposting.domain.type.AnalysisType;
 
 public interface AiEvalJobRepository extends JpaRepository<AiEvalJob, Long> {
-    Optional<AiEvalJob> findFirstByJobApplicationIdAndAnalysisTypeOrderByIdDesc(Long applicationId, AnalysisType analysisType);
 
+    Optional<AiEvalJob> findFirstByJobApplicationIdAndAnalysisTypeOrderByIdDesc(Long applicationId,
+            AnalysisType analysisType);
+
+    Optional<AiEvalJob> findFirstByRequestedByUserIdAndJobApplicationJobMasterIdOrderByIdDesc(Long userId,
+            Long jobMasterId);
 }
-
