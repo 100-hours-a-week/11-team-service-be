f1c1da5cc4044b73a847de7f313675c7820d8e73
hardNormalEasyMode <petequdwns12@naver.com>
2026-03-05 11:35:34 +0900

    test : 닉네임 동시성 문제 테스트

--- src/main/java/com/thunder11/scuad/common/config/AsyncConfig.java
+++ src/main/java/com/thunder11/scuad/common/config/AsyncConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
+// proxyTargetClass=true 추가 이유:
+// AiServiceClient가 AiComparePort 인터페이스를 구현하면서
+// Spring이 JDK 동적 프록시로 감싸 AiServiceClient 타입으로 주입이 불가능해짐.
+// CGLib 기반 프록시로 강제하면 구체 클래스 타입으로도 주입 가능.
+@EnableAsync(proxyTargetClass = true)
 public class AsyncConfig {
 }
