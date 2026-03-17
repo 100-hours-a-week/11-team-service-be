b95b388fdc51049864570e6309e9603cc439f625
CHAEGODA <chae_goda@naver.com>
2026-03-17 16:31:01 +0900

    feat: Redis 환경 설정 및 스프링 캐시 추상화 활성화 - spring-boot-starter-data-redis 의존성 추가 - application.yml 내 Redis 접속 정보(Host, Port, Password) 설정 추가 - RedisCacheManager 및 JSON 직렬화 설정을 위한 RedisCacheConfig 클래스 구현

---
+++ src/main/java/com/thunder11/scuad/infra/redis/config/RedisCacheConfig.java
@@ -0,0 +1,29 @@
+package com.thunder11.scuad.infra.redis.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues().entryTtl(Duration.ofDays(10))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(configuration).build();
+    }
+}
