b18a4568de6555bb097e9b4d01aa7ecc260acbdf
CHAEGODA <chae_goda@naver.com>
2026-03-22 22:05:26 +0900

    perf: AI 작업 복구 스케줄러 도입 및 RabbitMQ TTL 최적화

--- src/main/java/com/thunder11/scuad/infra/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/thunder11/scuad/infra/rabbitmq/config/RabbitMQConfig.java
@@ -29,34 +29,113 @@ public class RabbitMQConfig {
         return QueueBuilder.durable(QUEUE_EVALUATION)
                 .withArgument("x-dead-letter-exchange", "")
                 .withArgument("x-dead-letter-routing-key", QUEUE_EVALUATION + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
                 .build();
     }
+    @Bean
+    public Queue evaluationDeadLetterQueue() {
+        return QueueBuilder.durable(QUEUE_EVALUATION + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+    @Bean
+    public Queue evaluationDelayQueue() {
+        return QueueBuilder.durable(QUEUE_EVALUATION + ".delay")
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE_EVALUATION)
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+
     @Bean
     public Queue resumeQueue() {
         return QueueBuilder.durable(QUEUE_RESUME)
                 .withArgument("x-dead-letter-exchange", "")
                 .withArgument("x-dead-letter-routing-key", QUEUE_RESUME + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
                 .build();
     }
+    @Bean
+    public Queue resumeDeadLetterQueue() {
+        return QueueBuilder.durable(QUEUE_RESUME + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+    @Bean
+    public Queue resumeDelayQueue() {
+        return QueueBuilder.durable(QUEUE_RESUME + ".delay")
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE_RESUME)
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+
     @Bean
     public Queue portfolioQueue() {
         return QueueBuilder.durable(QUEUE_PORTFOLIO)
                 .withArgument("x-dead-letter-exchange", "")
                 .withArgument("x-dead-letter-routing-key", QUEUE_PORTFOLIO + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
                 .build();
     }
+    @Bean
+    public Queue portfolioDeadLetterQueue() {
+        return QueueBuilder.durable(QUEUE_PORTFOLIO + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+    @Bean
+    public Queue portfolioDelayQueue() {
+        return QueueBuilder.durable(QUEUE_PORTFOLIO + ".delay")
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE_PORTFOLIO)
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+
     @Bean
     public Queue comparisonQueue() {
         return QueueBuilder.durable(QUEUE_COMPARISON)
                 .withArgument("x-dead-letter-exchange", "")
                 .withArgument("x-dead-letter-routing-key", QUEUE_COMPARISON + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
                 .build();
     }
+    @Bean
+    public Queue comparisonDeadLetterQueue() {
+        return QueueBuilder.durable(QUEUE_COMPARISON + ".dead_letter")
+                .withArgument("x-message-ttl", 300000)
+                .build();
+    }
+    @Bean
+    public Queue comparisonDelayQueue() {
+        return QueueBuilder.durable(QUEUE_COMPARISON + ".delay")
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE_COMPARISON)
+                .withArgument("x-message-ttl", 30000)
+                .build();
+    }
+
     @Bean
     public Queue jobpostingQueue() {
         return QueueBuilder.durable(QUEUE_JOBPOSTING)
                 .withArgument("x-dead-letter-exchange", "")
                 .withArgument("x-dead-letter-routing-key", QUEUE_JOBPOSTING + ".dead_letter")
+                .withArgument("x-message-ttl", 30000)
+                .build();
+    }
+    @Bean
+    public Queue jobpostingDeadLetterQueue() {
+        return QueueBuilder.durable(QUEUE_JOBPOSTING + ".dead_letter")
+                .withArgument("x-message-ttl", 30000)
+                .build();
+    }
+    @Bean
+    public Queue jobpostingDelayQueue() {
+        return QueueBuilder.durable(QUEUE_JOBPOSTING + ".delay")
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE_JOBPOSTING)
+                .withArgument("x-message-ttl", 30000)
                 .build();
     }
 
@@ -84,6 +163,31 @@ public class RabbitMQConfig {
     @Bean
     public Binding bindJobPosting(Queue jobpostingQueue, DirectExchange aiExchange) {
         return BindingBuilder.bind(jobpostingQueue).to(aiExchange).with(QUEUE_JOBPOSTING);
+    }
+
+    @Bean
+    public Binding bindEvaluationDelay(Queue evaluationDelayQueue, DirectExchange aiExchange) {
+        return BindingBuilder.bind(evaluationDelayQueue).to(aiExchange).with(QUEUE_EVALUATION + ".delay");
+    }
+
+    @Bean
+    public Binding bindResumeDelay(Queue resumeDelayQueue, DirectExchange aiExchange) {
+        return BindingBuilder.bind(resumeDelayQueue).to(aiExchange).with(QUEUE_RESUME + ".delay");
+    }
+
+    @Bean
+    public Binding bindPortfolioDelay(Queue portfolioDelayQueue, DirectExchange aiExchange) {
+        return BindingBuilder.bind(portfolioDelayQueue).to(aiExchange).with(QUEUE_PORTFOLIO + ".delay");
+    }
+
+    @Bean
+    public Binding bindComparisonDelay(Queue comparisonDelayQueue, DirectExchange aiExchange) {
+        return BindingBuilder.bind(comparisonDelayQueue).to(aiExchange).with(QUEUE_COMPARISON + ".delay");
+    }
+
+    @Bean
+    public Binding bindJobPostingDelay(Queue jobpostingDelayQueue, DirectExchange aiExchange) {
+        return BindingBuilder.bind(jobpostingDelayQueue).to(aiExchange).with(QUEUE_JOBPOSTING + ".delay");
     }
 
     @Bean
